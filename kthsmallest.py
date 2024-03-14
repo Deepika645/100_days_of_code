14d93d27c77c9f45c5d5a716abf3f288ef261ed3
Deepika Sheshabutter <80688471+Deepika645@users.noreply.github.com>
2024-03-14 22:59:23 +0530

    """to find the kth smallest number"""

---
+++ kthsmallest.py
@@ -0,0 +1,14 @@
+"""to find the kth smallest number"""
+class Solution:
+    def kthSmallest(self,arr, l, r, k):
+        arr.sort()
+        return arr[k-1]
+    '''
+    arr : given array
+    l : starting index of the array i.e 0
+    r : ending index of the array i.e size-1
+    k : find kth smallest element and return using this function
+    '''
+
+
+        
