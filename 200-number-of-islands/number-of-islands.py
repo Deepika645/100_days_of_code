b151c6b7995bda27b8d4a95d38ff8ba65d4ef5a5
Deepika Sheshabutter <80688471+Deepika645@users.noreply.github.com>
2025-07-15 07:48:12 +0530

    Time: 327 ms (7.18%) | Memory: 25.4 MB (22.16%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,35 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rlen = len(grid)
+        clen = len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(row, col):
+            q = deque()
+            q.append((row, col))
+            visited.add((row, col))
+            while q:
+                m,n = q.popleft()
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+                for dr,dc in directions:
+                    r = m + dr
+                    c = n + dc
+                    if (r in range(rlen) and c in range(clen) and grid[r][c] == '1' and (r,c) not in visited):
+                        q.append((r,c))
+                        visited.add((r,c))
+
+        for i in range(rlen):
+            for j in range(clen):
+                if grid[i][j] == '1' and (i,j) not in visited:
+                    bfs(i,j)
+                    islands += 1
+        return islands
+        
+
+
+        
